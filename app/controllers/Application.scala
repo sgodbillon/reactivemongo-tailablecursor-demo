60403cad040e5d3f0edca63365a4884c2a647158
Stephane Godbillon <stephane@godbillon.fr>
2012-07-20 12:25:33 +0200

    Update example with the last version of the driver

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,8 +14,6 @@ import org.asyncmongo.api._
 import org.asyncmongo.handlers.{BSONReaderHandler, BSONWriter, BSONReader}
 import org.asyncmongo.handlers.DefaultBSONHandlers._
 import org.asyncmongo.protocol._
-import akka.dispatch.Future
-import akka.util.duration._
 
 object Application extends Controller {
   
@@ -25,50 +23,13 @@ object Application extends Controller {
 
   def watchCollection = WebSocket.using[JsValue] { request => 
     val coll = MongoAsyncPlugin.collection("acappedcollection")
+    // Inserts the received messages into the capped collection
     val in = Iteratee.foreach[JsValue] { json =>
       println("received " + json)
       coll.insert(json)
     }
-    val out = enumerate(Some(coll.find[JsValue, JsValue, JsValue](Json.obj(), None, 0, 0, QueryFlags.TailableCursor | QueryFlags.AwaitData)))
+    // Enumerates the capped collection
+    val out = Cursor.enumerate(coll.find[JsValue, JsValue, JsValue](Json.obj(), None, 0, 0, QueryFlags.TailableCursor | QueryFlags.AwaitData))
     (in, out)
-  }
-
-  def enumerate[T](futureCursor: Option[Future[Cursor[T]]]) :Enumerator[T] = {
-    var currentCursor :Option[Cursor[T]] = None
-    Enumerator.generateM {
-      if(currentCursor.isDefined && currentCursor.get.iterator.hasNext){
-        Promise.pure(Some(currentCursor.get.iterator.next))
-      } else if(currentCursor.isDefined && currentCursor.get.hasNext) {
-        val p = Promise[Option[T]]()
-        def periodicChecker(cursor: Cursor[T]) :Unit = {
-          if(cursor.iterator.hasNext) {
-            currentCursor = Some(cursor)
-            p.redeem(Some(cursor.iterator.next))
-          } else {
-            play.core.Invoker.system.scheduler.scheduleOnce(500 milliseconds)({
-              currentCursor.get.next.get.onSuccess {
-                case yop => periodicChecker(yop)
-              }
-            })
-          }
-        }
-        periodicChecker(currentCursor.get)
-        p
-      } else if(!currentCursor.isDefined && futureCursor.isDefined) {
-        new AkkaPromise(futureCursor.get.map { cursor =>
-          println("redeemed from first cursor")
-          currentCursor = Some(cursor)
-          if(cursor.iterator.hasNext) {
-            Some(cursor.iterator.next)
-          }
-          else {
-            None
-          }
-        })
-      } else {
-        println("Nothing to enumerate")
-        Promise.pure(None)
-      }
-    }
   }
 }
