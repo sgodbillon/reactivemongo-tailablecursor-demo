c76e2de9360008b59ec28716e4863ed23f3b6120
Stephane Godbillon <stephane@godbillon.fr>
2012-07-20 15:28:03 +0200

    update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,13 +7,13 @@ object ApplicationBuild extends Build {
     val appName         = "mongo-async-driver-app"
     val appVersion      = "1.0-SNAPSHOT"
 
-	val appDependencies = Seq(
-      "org.asyncmongo" %% "mongo-async-driver" % "0.1-SNAPSHOT"
+    val appDependencies = Seq(
+      "org.asyncmongo" %% "mongo-async-driver" % "0.1-SNAPSHOT",
+      "play.modules.mongodb" %% "play2-mongodb-async" % "0.1-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      //resolvers += ("localrepo" at "file:///Users/sgo/.ivy2/local")(Resolver.ivyStylePatterns)
-      resolvers += Resolver.file("local repository", file("/Users/sgo/.ivy2/local"))(Resolver.ivyStylePatterns)
+      resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
     )
 
 }
