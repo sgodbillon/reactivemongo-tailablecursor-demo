918ae8db12c3785a7950eecc05d6fc02b647bef1
Stephane Godbillon <stephane@godbillon.fr>
2012-08-27 11:55:21 +0200

    Update to ReactiveMongo and ReactiveMongo Plugin

--- README.md
+++ README.md
@@ -4,19 +4,17 @@ ReactiveMongo allows to enumerate a MongoDB cursor in a non-blocking and asynchr
 
 This demo shows how to stream the documents that are inserted into a capped collection through a WebSocket.
 It demonstrates the following features:
-+ Tailable Cursors with MongoAsync
++ Tailable Cursors with ReactiveMongo
 + Cursor Enumerators
 + Non-blocking I/O
 + Integration with Play JSON library (included in the Play ReactiveMongo Plugin)
-+ Integration of MongoAsync with Play 2 WebSocket API.
++ Integration of ReactiveMongo with Play 2 WebSocket API.
 
 When run, this sample allows to submit new documents via a WebSocket and inserts them into the Capped Collection. This Capped Collection is enumerated through the same Websocket, so any new document is eventually sent back to the client.
 
 ```scala
-    val tailableFlags = QueryFlags.TailableCursor | QueryFlags.AwaitData
-    
     def watchCollection = WebSocket.using[JsValue] { request => 
-      val coll = MongoAsyncPlugin.collection("acappedcollection")
+      val coll = ReactiveMongoPlugin.collection("acappedcollection")
       
       // Inserts the received messages into the capped collection
       val in = Iteratee.foreach[JsValue] { json =>
@@ -24,8 +22,10 @@ When run, this sample allows to submit new documents via a WebSocket and inserts
         coll.insert(json)
       }
       // Enumerates the capped collection
-      val out = Cursor.enumerate(
-        coll.find[JsValue, JsValue, JsValue](Json.obj(), flags = tailableFlags))
+      val out = {
+        val cursor = coll.find(Json.obj(), QueryOpts().tailable.awaitData)
+        cursor.enumerate
+      }
       
       // We're done!
       (in, out)
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,40 +9,36 @@ import play.api._
 import play.api.mvc._
 import play.api.Play.current
 import play.api.libs.json._
-import play.api.libs.concurrent._
 import play.api.libs.iteratee._
 
-import play.modules.mongodb._
-import play.modules.mongodb.PlayBsonImplicits._
+import play.modules.reactivemongo._
+import play.modules.reactivemongo.PlayBsonImplicits._
 
-import org.asyncmongo.api._
-import org.asyncmongo.handlers.{BSONReaderHandler, BSONWriter, BSONReader}
-import org.asyncmongo.handlers.DefaultBSONHandlers._
-import org.asyncmongo.protocol._
+import reactivemongo.api._
+import reactivemongo.bson.handlers.DefaultBSONHandlers.DefaultBSONReaderHandler
 
-import akka.util.Timeout
-import akka.util.duration._
+import scala.concurrent.{ExecutionContext, Future}
 
-object Application extends Controller {
+object Application extends Controller with MongoController {
+
   def index = Action {
     Ok(views.html.index())
   }
 
-  val tailableFlags = QueryFlags.TailableCursor | QueryFlags.AwaitData
-
   def watchCollection = WebSocket.using[JsValue] { request =>
-    val coll = MongoAsyncPlugin.collection("acappedcollection")
+    val coll = ReactiveMongoPlugin.collection("acappedcollection")
 
     // Inserts the received messages into the capped collection
     val in = Iteratee.foreach[JsValue] { json =>
       println("received " + json)
       coll.insert(json)
     }
+
     // Enumerates the capped collection
-    val out = Cursor.enumerate(
-      // make sure the primary has been discovered.
-      coll.connection.waitForPrimary(Timeout(1 seconds)).flatMap(_ =>
-        coll.find[JsValue, JsValue, JsValue](Json.obj(), flags = tailableFlags)))
+    val out = {
+      val cursor = coll.find(Json.obj(), QueryOpts().tailable.awaitData)
+      cursor.enumerate
+    }
 
     // We're done!
     (in, out)
