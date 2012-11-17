bf6637b6d9965b2cf454dec3cdf141f5c7b6ab97
Stephane Godbillon <stephane@godbillon.fr>
2012-11-17 16:01:08 +0100

    Update to Scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "mongo-async-driver-app"
+    val appName         = "ReactiveMongo-Demo-TailableCursors"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "reactivemongo" %% "reactivemongo" % "0.1-SNAPSHOT",
-      "play.modules.reactivemongo" %% "play2-reactivemongo" % "0.1-SNAPSHOT"
+      "reactivemongo" %% "reactivemongo" % "0.1-SNAPSHOT" cross CrossVersion.full,
+      "play.modules.reactivemongo" %% "play2-reactivemongo" % "0.1-SNAPSHOT" cross CrossVersion.full
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
     )
 
